94f4a245ff450a4d2d75090d959b056604e5c847
Flavio Henrique <f.henrique.n.l@gmail.com>
2020-05-19 20:28:29 -0300

    criando uma versão forfun.

--- src/Database/connection.js
+++ src/Database/connection.js
@@ -1,14 +1,15 @@
 import firebase from "firebase"
 
 let firebaseConfig = {
-    apiKey: "AIzaSyAW3VMDiSzXlnrScbYATbWskasIVy_cc7U",
-    authDomain: "saudearapiraca-farmaciateste.firebaseapp.com",
-    databaseURL: "https://saudearapiraca-farmaciateste.firebaseio.com",
-    projectId: "saudearapiraca-farmaciateste",
-    storageBucket: "saudearapiraca-farmaciateste.appspot.com",
-    messagingSenderId: "176365758465",
-    appId: "1:176365758465:web:d266a9f5fca4b1db3d7800"
-  };
+  apiKey: "AIzaSyAWC_I31Lg0T_b2Suqwc4SH5SRClkkURZg",
+  authDomain: "projeto-teste-67eac.firebaseapp.com",
+  databaseURL: "https://projeto-teste-67eac.firebaseio.com",
+  projectId: "projeto-teste-67eac",
+  storageBucket: "projeto-teste-67eac.appspot.com",
+  messagingSenderId: "307914561993",
+  appId: "1:307914561993:web:0b76da4f7e18601c2bf018",
+  measurementId: "G-C01VNS8EVE"
+};
 // Initialize Firebase
 firebase.initializeApp(firebaseConfig);
 
